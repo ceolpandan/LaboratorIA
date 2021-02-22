fa479d74b043a663df58a7779fdd73cb61fb078c
ceolpandan <48345886+ceolpandan@users.noreply.github.com>
2021-02-22 12:39:15 +0200

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from skimage import io
+
 # a)
 print("# a)")
-
 files = os.listdir("images")
 filesArray = [np.load("images/" + files[i]) for i in range(0, len(files))]
 images = np.array(filesArray)
@@ -12,7 +12,6 @@ print(images.shape)
 
 # b)
 print("# b)")
-
 sum = 0
 for image in images:
     sum += np.sum(image)
@@ -22,14 +21,12 @@ print(np.sum(images))
 
 # c)
 print("# c)")
-
 for image in images:
     print(np.sum(image))
 
 
 # d)
 print("# d)")
-
 maxIndex = -1
 maxSum = max(np.sum(image) for image in images)
 for idx,image in enumerate(images):
